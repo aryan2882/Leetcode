0ba9d83a8553d74ca5ea4289875e63a8eef4632c
Aryan RAJ <131025781+aryan2882@users.noreply.github.com>
2024-11-02 12:12:09 +0530

    Time: 2 ms (78%), Space: 17.5 MB (28.71%) - LeetHub

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        priority_queue<pair<int,int>> pq;
+        unordered_map<int,int> mp;
+        for(int i:nums){
+            mp[i]++;
+        }
+        for(const auto& i:mp){
+            pq.push({i.second,i.first});
+        }
+        vector<int> ans;
+        for(int i=0;i<k;i++){
+            ans.push_back(pq.top().second);
+            pq.pop();
+        }
+        return ans;
+
+    }
+};
